4779cf111ee65eb03a5fe17c233b251bffaa7a9d
Rose-Aline <116867946+Rose-Aline@users.noreply.github.com>
2023-06-01 11:04:27 +0200

    index responsive

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,22 +4,21 @@
 <template>
     <main class="bg-backgroundbeige">
 
-  <div>
-    <img src="../img/Robert_Demarchy_Severity_light.webp" alt="Image" class="w-full drop-shadow-[0_10px_10px_rgba(0,0,0,0.30)]	">
-  </div>
+      <div class="lg:flex">
+        <div class="lg:w-1/2">
+          <img src="../img/Robert_Demarchy_Severity_light.webp" alt="Image" class="w-full h-full drop-shadow-[0_10px_10px_rgba(0,0,0,0.30)]	object-cover ">
+        </div>
 
-  <div class="pt-8 px-16 pr-20 bg-beige">
-    <h1 class="uppercase">Le pictorialisme</h1>
-    <h2>Exposition du 13 Avril au 21 Juin 2021</h2>
-    <p class="pt-3">Le pictorialisme est un mouvement esthétique international qui caractérise la photographie à partir de 1885 environ.</p>
-    <p class="pt-3">Les artistes pictorialistes souhaitent « dépasser la simple imitation mécanique pour ériger la photographie en un art autonome et distinct.</p>
-      <button class="btn my-10">
-         Réserver un billet
-      </button>
-
-  </div>
-
-
+        <div class="pt-8 px-16 pr-20 bg-beige lg:w-1/2">
+          <h1 class="uppercase">Le pictorialisme</h1>
+          <h2>Exposition du 13 Avril au 21 Juin 2021</h2>
+          <p class="pt-3">Le pictorialisme est un mouvement esthétique international qui caractérise la photographie à partir de 1885 environ.</p>
+          <p class="pt-3">Les artistes pictorialistes souhaitent « dépasser la simple imitation mécanique pour ériger la photographie en un art autonome et distinct.</p>
+            <button class="btn my-10">
+              Réserver un billet
+            </button>
+          </div>
+        </div>
     </main>
 </template>
 
