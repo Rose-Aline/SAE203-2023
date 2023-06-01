7928c44de1e0b9fcf6458b73287d7af0f3a92504
Rose-Aline <116867946+Rose-Aline@users.noreply.github.com>
2023-06-01 13:32:16 +0200

    Billeterie responsive

--- src/pages/Billeterie.vue
+++ src/pages/Billeterie.vue
@@ -1,36 +1,38 @@
 <script setup lang="ts">
 </script>
 <template>
-    
-    <main class="bg-backgroundbeige">
-        <div class="text-center	py-2 pt-10">
-            <h1 class="uppercase">Le pictorialisme</h1>
-            <h2>Exposition du 13 Avril au 21 Juin 2021</h2>
-        </div>
-        <img src="../img/Robert_Demarchy_Severity_light.webp" alt="Image" class="w-full h-80 drop-shadow-[0_10px_10px_rgba(0,0,0,0.30)]	object-cover ">
 
-        <div class="pt-10 px-10">
-            <h2 class="font-bold ">Fondation Cartier, 261 Bd Raspail, 75014 Paris</h2>
-            <p> <a class="text-orange">Tous les jours de 11h à 20h, sauf le lundi. </a> Nocturne le mardi, jusqu'à 22h.</p>
-            <p class="pt-5"> La fermeture des salles débute à 19h45 (21h45 les mardis)</p>
-            
-            <div class="text-2xl">
-                <h1 class="uppercase pt-10">tarifs</h1>
-                <p class="pt-5">Plein tarif ⇒ <a class="text-orange">11€</a></p>
-                <p>Tarif réduit ⇒ <a class="text-orange">7,50€</a></p>
-                <p>Réduction 13 - 25 ans ⇒ <a class="text-orange">5€</a></p>
-                <p>Fondation Cartier Pass⇒ <a class="text-orange">18€ - 50€</a></p>
-                <p class="pt-5"> Moins de 13 ans, Laissez-passer annuel, carte Icom, carte de presse, carte d'invalidité, Fondation Cartier Pass, Etudiants ⇒ <a class="text-orange">Gratuit</a></p>
+    <main class="bg-backgroundbeige">
+        <div class="lg:pb-10"></div>
+        <div class="lg:flex lg:flex-row-reverse lg:mx-12 lg:mr-56 lg:gap-20 lg:bg-beige ">
+            <div class="text-center	lg:text-left py-2 pt-10 lg:w-1/2">
+                <h1 class="uppercase">Le pictorialisme</h1>
+                <h2>Exposition du 13 Avril au 21 Juin 2021</h2>
+             </div>
+              <img src="../img/Robert_Demarchy_Severity_light.webp" alt="Image" class="lg:w-1/2 lg:h-44 w-full h-80 drop-shadow-[0_10px_10px_rgba(0,0,0,0.30)]	object-cover ">
+            </div>
+            <div class="pt-10 px-10">
+                <h2 class="font-bold ">Fondation Cartier, 261 Bd Raspail, 75014 Paris</h2>
+                <p> <a class="text-orange">Tous les jours de 11h à 20h, sauf le lundi. </a> Nocturne le mardi, jusqu'à 22h.</p>
+                <p class="pt-5"> La fermeture des salles débute à 19h45 (21h45 les mardis)</p>
+                
+                <div class="text-2xl">
+                    <h1 class="uppercase pt-10">tarifs</h1>
+                    <p class="pt-5">Plein tarif ⇒ <a class="text-orange">11€</a></p>
+                    <p>Tarif réduit ⇒ <a class="text-orange">7,50€</a></p>
+                    <p>Réduction 13 - 25 ans ⇒ <a class="text-orange">5€</a></p>
+                    <p>Fondation Cartier Pass⇒ <a class="text-orange">18€ - 50€</a></p>
+                    <p class="pt-5"> Moins de 13 ans, Laissez-passer annuel, carte Icom, carte de presse, carte d'invalidité, Fondation Cartier Pass, Etudiants ⇒ <a class="text-orange">Gratuit</a></p>
+                </div>
+
+                <a href="https://fondation-cartier.tickeasy.com/fr-FR/tarifs">
+                <button class="btn my-10">
+                    Réserver
+                </button>
+                </a>
+
             </div>
 
-            <a href="https://fondation-cartier.tickeasy.com/fr-FR/tarifs">
-              <button class="btn my-10">
-                Réserver
-              </button>
-            </a>
-
-        </div>
-        
   </main>
 </template>
 
